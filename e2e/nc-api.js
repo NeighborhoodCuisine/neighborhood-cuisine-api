a7a5d332b8a8b07e071e59557b50aa895151c4c9
Sven Mkw <sven.mkw@gmail.com>
2016-05-29 23:27:53 +0200

    added getRecipesFromIngredients api call extended e2e tests

--- e2e/nc-api.js
+++ e2e/nc-api.js
@@ -24,12 +24,36 @@ describe('E2E Neighborhood Cuisine Api', () => {
   it('should retrieve recipe by id', (done) => {
     api.getRecipeCard(42)
       .then(({ data }) => {
-        expect(data.summary).to.be.instanceOf('string')
-        expect(data.extendedIngredients).to.be.instanceOf('object')
+        expect(data.summary).to.be.a.string
+        expect(data.extendedIngredients).to.be.an.array
+        expect(data.extendedIngredients).to.have.a.lengthOf(10)
+
+        const i = data.extendedIngredients[0]
+        expect(i).to.have.property('amount')
+        expect(i).to.have.property('name')
+        expect(i).to.have.property('unit')
+        expect(i).to.have.property('unitLong')
         done()
       })
-      .catch(e => {
-        done(e)
+      .catch(done)
+  })
+
+  it('should retrieve recipes from ingredients', (done) => {
+    const ingredients = ['honey', 'beef']
+    api.getRecipesFromIngredients(ingredients)
+      .then(({ data }) => {
+        expect(data).to.have.length.of.at.least(1)
+        expect(data[0]).to.have.property('image_url')
+        expect(data[0]).to.have.property('ingredients')
+        expect(data[0]).to.have.property('missing_ingredients')
+        expect(data[0]).to.have.property('recipe_id')
+        expect(data[0]).to.have.property('title')
+        expect(data[0].ingredients).to.be.an.array
+        expect(data[0].ingredients[0]).to.have.property('name')
+        expect(data[0].ingredients[0]).to.have.property('aisle')
+        expect(data[0].ingredients[0]).to.have.property('image')
+        done()
       })
+      .catch(done)
   })
 })
